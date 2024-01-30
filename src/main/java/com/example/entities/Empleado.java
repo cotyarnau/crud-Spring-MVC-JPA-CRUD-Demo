1f78faab914c75a48c0542741c37ccf0d89a2a5e
cotyarnau <cotyarnau@gmail.com>
2024-01-30 16:36:04 +0100

    Estado inicial del proyecto

---
+++ src/main/java/com/example/entities/Empleado.java
@@ -0,0 +1,59 @@
+package com.example.entities;
+
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import org.hibernate.annotations.ManyToAny;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="empleados")
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Empleado implements Serializable {
+
+    private static final long serialVersionUID = 1L; 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id; 
+
+    private String nombre;
+    private String primerApellido;
+    private String segundoApellido;
+  
+    
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private double salario;
+    private LocalDate fechaAlta;
+
+    @Enumerated(EnumType.STRING)
+    private Genero genero; 
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    private Departartamento departamento;
+    
+     
+}
